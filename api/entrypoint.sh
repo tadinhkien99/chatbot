4f8794a86c241da4573835fb8619221de29b3ac4
Kuro <tadinhkien99@gmail.com>
2025-02-22 15:31:57 +0700

    feat: init

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$SERVICE" = "app" ]; then
-    exec uvicorn api.main:app --host 0.0.0.0 --port 8009 --workers 1
+    exec uvicorn app:app --host 0.0.0.0 --port 8009 --workers 1
 elif [ "$SERVICE" = "worker" ]; then
-    exec celery -A api.tasks worker --loglevel=info
+    exec celery -A tasks worker --loglevel=info
 else
     exec "$@"
 fi
